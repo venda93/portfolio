e2554c5750758f20a46b621417e8265de7089bc5
venda93 <34418409+venda93@users.noreply.github.com>
2020-10-30 10:50:46 +0100

    deleted

--- app.js
+++
@@ -1,9 +0,0 @@
-const slider = document.querySelector(".slider");
-const navbar = document.querySelector(".navbar");
-const tl = new TimelineMax();
-
-tl.fromTo(
-slider,1.2, {x:"-100%"}, {x:"0%", ease: Power2.easeInOut}
-)
-.fromTo(navbar,1.2,{y:"-100%"}, {y:"0%",ease: Power2.easeInOut}, "-=1.2"
-);
